4b64594ed96d2e40f14ed3b2566df602b5048b94
wenbox360 <101437759+wenbox360@users.noreply.github.com>
2023-11-18 13:29:42 -0500

    logo v3

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
+import Image from 'next/image'
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Greyzone</h1>
-      <p>Politics for Dummies</p>
+    <div id="parent">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv='cache-control' content='no-cache' />
+        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet' />
+      </Head>
+
+      <header id="landing-header">
+        <Image src="/Logo_V2-transparent.png" width={300} height={300} />
+      </header>
+
+      <h1 className="landing-heading">landing-heading 1</h1>
+      <h2 className="landing-heading">landing-heading 2</h2>
+
+      <button className="button">Button</button>
+      <button className="button2">Button 2</button>
     </div>
   )
 }
