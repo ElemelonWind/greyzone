c0b77f975dbde79bc4192efe23e5ed7d6b07fccf
ElemelonWind <c.yang.2023@gmail.com>
2023-11-18 09:27:26 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
+import { Dosis } from 'next/font/google'
+
+const dosis = Dosis({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component className={dosis.className} {...pageProps} />
 }
