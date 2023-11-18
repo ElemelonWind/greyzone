89a4b2ceee4806a5fba91ae6329cff4e07fc61ab
ElemelonWind <c.yang.2023@gmail.com>
2023-11-18 12:06:38 -0500

    chat styles

--- pages/chat.js
+++ pages/chat.js
@@ -4,7 +4,9 @@ import ScrollContainer from '@/components/ScrollContainer';
 
 export default function Chat() {
     const [inputValue, setInputValue] = useState('');
-    const [chatLog, setChatLog] = useState([]);
+    const [chatLog, setChatLog] = useState([
+        { type: 'bot', message: 'Hi, I\'m Olive, your One-Zone chat assistant. How can I help you?'}
+    ]);
     const [isLoading, setIsLoading] = useState(false);
 
     const handleSubmit = (event) => {
@@ -53,40 +55,41 @@ export default function Chat() {
     ))
 
     return (
-        <div className="container mx-auto max-w-[700px]">
-            <div className="flex flex-col h-screen bg-gray-900">
-                <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 
-                    text-transparent bg-clip-text text-center py-3 font-bold text-6xl">
-                    Greyzone Chat
-                </h1>
-                <div className="flex-grow p-6">
-                <div className="flex flex-col space-y-4"
-                    style={{ height: '75vh' }}
-                >
-                    <ScrollContainer children={messages} />
-                    { isLoading &&
-                        <div key={chatLog.length} className="flex justify-start">
-                            <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
-                                <TypingAnimation />
+        <div className="chat-page">
+            <div className="chat container mx-auto max-w-[700px]">
+                <div className="flex flex-col">
+                    <h1 className="text-center py-3 font-bold text-6xl">
+                        Talk to Olive!
+                    </h1>
+                    <div className="messages flex-grow p-6">
+                    <div className="flex flex-col space-y-4"
+                        style={{ height: '65vh' }}
+                    >
+                        <ScrollContainer children={messages} />
+                        { isLoading &&
+                            <div key={chatLog.length} className="flex justify-start">
+                                <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
+                                    <TypingAnimation />
+                                </div>
                             </div>
+                        }
                         </div>
-                    }
                     </div>
+                    <form onSubmit={handleSubmit} className="flex-none p-6">
+                        <div className="flex rounded-lg border border-gray-700 bg-gray-800">  
+                            <input type="text" className="flex-grow px-4 py-3 bg-transparent 
+                                text-white focus:outline-none" placeholder="Type your message..." 
+                                value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                                
+                            <button type="submit" className="rounded-lg px-4 py-2 
+                                text-white font-semibold focus:outline-none
+                                transition-colors duration-300">
+                                Send
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <form onSubmit={handleSubmit} className="flex-none p-6">
-                <div className="flex rounded-lg border border-gray-700 bg-gray-800">  
-                <input type="text" className="flex-grow px-4 py-2 bg-transparent 
-                    text-white focus:outline-none" placeholder="Type your message..." 
-                    value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-                    
-                <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 
-                    text-white font-semibold focus:outline-none hover:bg-purple-600 
-                    transition-colors duration-300">
-                    Send
-                </button>
-                    </div>
-                </form>
-                </div>
+            </div>
         </div>
     )
 }
